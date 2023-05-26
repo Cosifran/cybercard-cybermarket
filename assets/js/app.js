76e8cedf0de06311ffee52298c651ab83f5d5c2c
Francisco Front <90176645+Cosifran@users.noreply.github.com>
2023-05-26 18:02:25 -0500

    26/05/23 update

--- assets/js/app.js
+++ assets/js/app.js
@@ -182,3 +182,23 @@ navigation: {
     },
   },
 });
+
+//slide cupon
+var swiper = new Swiper(".slidePuntos", {
+  slidesPerView: 2,
+  spaceBetween: 10,
+navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 0,
+    },
+  },
+});
